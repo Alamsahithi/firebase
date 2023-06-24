63a5c0f816c7d1afd845eced1beabda8afcd3727
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-06-24 18:31:02 +0530

    Update promiseall.js

--- promiseall.js
+++ promiseall.js
@@ -1,70 +1,40 @@
-function updateLastUserActivityTime(post, lastActivityTime) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        lastActivityTime = Date.now();
-        resolve(lastActivityTime);
-      }, 1000);
-    });
+function buyCar() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Car");
+    }, 1000);
+  });
+}
+
+function goToManali() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Manali");
+    }, 2000);
+  });
+}
+
+function reachDestination(destination) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Reached ${destination}`);
+    }, 1500);
+  });
+}
+
+async function manaliTrip() {
+  try {
+    const car = await buyCar();
+    console.log("Buy a", car);
+
+    const destination = await goToManali();
+    console.log("Let's go to", destination);
+
+    const reachedManali = await reachDestination(destination);
+    console.log(reachedManali);
+  } catch (error) {
+    console.error("Error:", error);
   }
-  
-  function createPost(post) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(post);
-      }, 2000);
-    });
-  }
-  
-  function deletePost(post) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(post);
-      }, 1500);
-    });
-  }
-  
-  const user = {
-    name: "John",
-    lastActivityTime: new Date("Fri Sep 02 2022 17:21:05 GMT+05:30")
-  };
-  
-  const posts = [];
-  
-  function logPostsAndActivity(posts, lastActivityTime) {
-    console.log("Posts:");
-    posts.forEach((post, index) => {
-      console.log(`post${index + 1}: ${post}`);
-    });
-    console.log("User last activity time:", new Date(lastActivityTime).toString());
-  }
-  
-  function createAndDeletePost() {
-    const post = "New post";
-  
-    console.log("Before creating post, user last activity time:", user.lastActivityTime.toString());
-  
-    Promise.all([createPost(post), updateLastUserActivityTime(post, user.lastActivityTime.getTime())])
-      .then(([createdPost, updatedLastActivityTime]) => {
-        posts.push(createdPost);
-        user.lastActivityTime = updatedLastActivityTime;
-        console.log("After creating post", createdPost);
-        logPostsAndActivity(posts, updatedLastActivityTime);
-        return deletePost(posts[posts.length - 1]);
-      })
-      .then(deletedPost => {
-        const deletedIndex = posts.findIndex(post => post === deletedPost);
-        if (deletedIndex !== -1) {
-          posts.splice(deletedIndex, 1);
-        }
-       // console.log("New Set of Posts:");
-        posts.forEach((post, index) => {
-          console.log(`post${index + 1}: ${post}`);
-        });
-      })
-      .catch(error => {
-        console.error("Error:", error);
-      });
-  }
-  
-  createAndDeletePost();
-  
+}
+
+manaliTrip();
